f6bdc6ee435629015bdc45a098478034b94ef1a1
German Arcila <germanarcila@gmail.com>
2016-03-16 14:27:18 -0400

    commit

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
 
+	belongs_to :store
+
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :hourly_rate, numericality: {within: (40..200)}
+  #validates :  employee must always have a store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,19 @@
 class Store < ActiveRecord::Base
 
+	has_many :employees
+
+	validates :name, presence: true
+	validates :name, length: {minimum: 3}
+	validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+	
+	validate :must_carry_apparel
+
+	def must_carry_apparel
+		if mens_apparel.eql? nil
+			errors.add(:mens_apparel, "must carry womens or mens apparel")
+		end
+		if womens_apparel.eql? nil
+			errors.add(:womens_apparel, "must carry womens or mens apparel")			
+		end
+	end
 end
